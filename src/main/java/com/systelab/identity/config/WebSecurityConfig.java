0aba98c6635f1810e419b2068cd84d7749118d0f
alfonsserra <alfonsoserra@gmail.com>
2018-12-09 07:24:16 +0100

    Remove Method Security

--- src/main/java/com/systelab/identity/config/WebSecurityConfig.java
+++ src/main/java/com/systelab/identity/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.systelab.identity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +9,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
